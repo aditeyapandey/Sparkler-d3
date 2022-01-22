27b40aa8d49e53f6c863d7e400517c03b41edb3e
aditeyapandey <pandey.ad@husky.neu.edu>
2022-01-22 11:27:07 -0500

    adding dataloader

---
+++ js/dataloader.js
@@ -0,0 +1,9 @@
+(function () {
+  dataLoader = {};
+  console.log("test")
+  dataLoader.loadData = function (file="sex")
+  {
+//    use d3.tsv
+// d3.tsv("data/GSE96583_batch2.sub.tsv").then(function (data) {}
+}
+})();
